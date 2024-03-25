a9f7b2f7885e2a36512f8d60caff6fb580b87b32
ileana Brotsky <ileana.brotsky-ls@alitaware.com>
2024-03-25 17:27:43 -0300

    structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
 import Footer from "./components/layout/footer/Footer";
 import Navbar from "./components/layout/navbar/Navbar";
-import Counter from "./components/common/counter/Counter";
+
 function App() {
   return (
     <div>
-    
+    <Navbar/>
+    <ItemListContainer/>
+    <Footer/>
     </div>
   );
 }
