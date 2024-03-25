75c88a41dfb7c1f3a1586833e9f0e512d254db1b
ileana Brotsky <ileana.brotsky-ls@alitaware.com>
2024-03-25 17:12:09 -0300

    project created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
+import Footer from "./components/layout/footer/Footer";
+import Navbar from "./components/layout/navbar/Navbar";
+import Counter from "./components/common/counter/Counter";
+function App() {
+  return (
+    <div>
+    
+    </div>
+  );
+}
+
+export default App;
+
+
+
