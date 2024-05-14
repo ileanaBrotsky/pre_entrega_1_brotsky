05a195aa4110fb4f8e1c3a8f45231861773a239e
ileana Brotsky <ileana.brotsky-ls@alitaware.com>
2024-05-14 16:07:10 -0300

    counter added

---
+++ src/components/common/counter/CounterContainer.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+import CounterPresentacional from "./CounterPresentacional";
+
+export const CounterContainer = ( {stock, initial=1, onAdd } ) => {
+  
+    const [contador, setContador] = useState(initial);
+
+  const sumar = () => {
+    if( contador < stock ){
+      setContador(contador + 1);
+    }else{
+      alert("maximo en stock")//CAMBIAR ALERT POR ALGO MAS LINDO
+    }
+  };
+
+  const restar = () => {
+    if (contador > 1) {
+      setContador(contador - 1);
+    } else {
+      alert("no podes menos de 1");
+    }
+  };
+
+
+
+  let objectProps = {
+    restar,
+    sumar,
+    contador,
+    onAdd
+ 
+  };
+
+  return (
+    <CounterPresentacional {...objectProps} />
+  );
+};
