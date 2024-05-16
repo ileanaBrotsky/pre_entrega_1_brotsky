09c1b227c62d405e406a7429ac748038e9a4a1b5
ileana Brotsky <ileana.brotsky-ls@alitaware.com>
2024-05-16 20:32:26 -0300

    styles changed

--- src/components/pages/itemListContainer/ItemList.jsx
+++ src/components/pages/itemListContainer/ItemList.jsx
@@ -3,11 +3,7 @@ import ProductCard from "../../common/productCard/ProductCard.jsx";
 
 export default function ItemList({ items,error }) {
   return (
-    <Grid container sx={{ backgroundColor: "#D9D5CB", paddingBottom: "80px"}}>
-      <Grid item xs={12} sx={{ paddingLeft: "20px" }}>
-        {" "}
-        <h3>Bienvenido</h3>
-      </Grid>
+    <Grid container sx={{  paddingBottom: "80px"}}>
       <Grid item xs={12} sx={{display: "flex", justifyContent: "center",flexWrap: "wrap", gap: "20px",padding:"1rem" }}>
         {items.map(({id, title,price,description,img}) => (
           <ProductCard
